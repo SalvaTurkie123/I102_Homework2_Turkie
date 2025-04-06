1fba739a01ba6297212d2c191062291fc8a46030
SalvaTurkie123 <sturkie@udesa.edu.ar>
2025-04-06 13:20:33 -0300

    Ej 3 Terminado.

--- ej3/main.cpp
+++ ej3/main.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
 #include "entero.h"
 #include "real.h"
 #include "complejo.h"
+#include <iostream>
 
 using namespace std;
 
 void probarNumeros() {
-
-    Entero e1(5), e2(3);
-    Real r1(4.5), r2(1.5);
+     
+    // Crear objetos de las clases Entero, Real y Complejo
+    Entero e1(5), e2(-3);
+    Real r1(4.5), r2(0.0);
     Complejo c1(2, 3), c2(1, -4);
 
+    // Operaciones entre números del mismo tipo
+    cout << "=== Operaciones entre números del mismo tipo ===" << endl;
+    
     cout << "Entero: " << e1.toString() << " + " << e2.toString() 
          << " = " << e1.suma(e2)->toString() << endl;
 
